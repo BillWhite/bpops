c8f83cc7fc12aab933152f1485e68a9037b513ac
poppa <bill.white@griggsinst.com>
2022-09-10 19:29:02 -0700

    Added some support for force-command. Not clearly right yet.

--- scripts/make_user_certificate.sh
+++ scripts/make_user_certificate.sh
@@ -5,9 +5,10 @@ CA_FILE=user_ca_key
 OUTFILE=ssh_certificate
 PASSPHRASE=
 TARFILE=/tmp/user_cert.tgz
+EXPIRY="always:forever"
 # options may be followed by one colon to indicate they have a required argument
-SHORT_OPTS="uhdxep:U:M:D:t:f:n:W:A:C:V:i:z:"
-LONG_OPTS="user,host,debug,xdebug,echo,priority:,user-name:,user-home:,domain-list:,cipher:,tarfile:,outfile:,principals:,workdir:,authority:,ca-dir:,expiration:,key-id:,serial-number:"
+SHORT_OPTS="uhdxep:U:M:D:t:f:n:W:A:C:V:i:z:O:"
+LONG_OPTS="user,host,debug,xdebug,echo,priority:,user-name:,user-home:,domain-list:,cipher:,tarfile:,outfile:,principals:,workdir:,authority:,ca-dir:,expiration:,key-id:,serial-number:command:"
 if ! options=$(getopt -u -o "$SHORT_OPTS" -l "$LONG_OPTS" -- "$@")
 then
     # something went wrong, getopt will put out an error message for us
@@ -21,10 +22,9 @@ USER_NAME="$USER"
 USER_HOME="$HOME"
 KEY_ID="kid"
 SERIAL_NUMBER="0000"
-PRIORITY=10
+PRIORITY=50
 while [ $# -gt 0 ]
 do
-  echo "opts: $1 $2"
   case $1 in
     -u|--user)           KEY_KIND=; KIND_NAME="User";;
     -h|--host)           KEY_KIND=-h; KIND_NAME="Host"; CA_FILE=host_ca_key ;;
@@ -48,6 +48,7 @@ do
     -C|--ca-dir)         CA_DIR="$2"; shift;;
     -V|--expiration)     EXPIRY="$2"; shift;;
     -p|--priority)       PRIORITY="$2"; shift;;
+    -O|--command)        FORCE_COMMAND="-O force-command=\'$2\'"; shift;;
     (--)                 shift; break;;
     (-*) echo "$0: error - unrecognized option $1" 1>&2; exit 1;;
     (*) break;;
@@ -80,16 +81,16 @@ fi
 mkdir -p $WORKDIR/.ssh/ssh_config.d
 # 1. Create a new key pair to sign.
 echo $0: Creating a user key pair in $OUTFILE and ${OUTFILE}.pub
-$ECHO ssh-keygen -t "${CIPHER}" $EXPIRY -f "$WORKDIR/.ssh/$OUTFILE" -N "$PASSPHRASE"
+$ECHO ssh-keygen -t "${CIPHER}" -f "$WORKDIR/.ssh/$OUTFILE" -N "$PASSPHRASE"
 # 2. Sign the new key pair.
 echo $0: Signing public key ${OUTFILE}.pub
 if [ -z "$PRINCIPALS" ] ; then
     PRINCIPALS="$USER_NAME"
 fi
-$ECHO ssh-keygen -s "${CA_PATH}" -I "${KEY_ID}" -z "${SERIAL_NUMBER}" \
+$ECHO ssh-keygen -s "${CA_PATH}" -V "$EXPIRY" -I "${KEY_ID}" -z "${SERIAL_NUMBER}" \
 	-n "$PRINCIPALS" "$WORKDIR/.ssh/${OUTFILE}.pub"
 # 3. Create a stanza for $WORKDIR/.ssh_config.d.
-cat > $WORKDIR/.ssh/ssh_config.d/$PRIORITY.ssh_certificate_${OUTFILE} << EOF
+cat > $WORKDIR/.ssh/ssh_config.d/$PRIORITY.${OUTFILE} << EOF
 Host $DOMAIN_LIST
         Hostname %h
         User $USER_NAME
@@ -98,7 +99,7 @@ Host $DOMAIN_LIST
 EOF
 # 4. Packing up the tar file.
 echo $0: Packing it all up in ${OUTFILE}.tgz
-$ECHO tar --directory "$WORKDIR" --create --gzip --file ${TARFILE}.tgz .ssh
+$ECHO tar --directory "$WORKDIR" --create --gzip --file ${OUTFILE}.tgz .ssh
 # N. Delete the workdir.
 if [ -z "$SAVEWORKDIR" ] ; then
     echo $0: Deleting working $WORKDIR
